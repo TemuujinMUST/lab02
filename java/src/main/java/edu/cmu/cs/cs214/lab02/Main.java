733194d74d16ce423c685f63481573bd23d9ad8c
TemuujinMUST <B232270020@must.edu.mn>
2025-01-28 13:46:50 +0800

    fix: implemented encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,22 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape square = new Square(4);
+        Shape circle = new Circle(5);
 
-        Renderer renderer = new Renderer(rectangle);
+        printShape(rectangle);
+        printShape(square);
+        printShape(circle);
+    }
 
-        renderer.draw();
+    public static void printShape(Shape shape) {
+        System.out.println("Shape printed\n" + "The area of " + shape.getShapeName() + " is " + shape.getArea());
     }
 }
